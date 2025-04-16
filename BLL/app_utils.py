64f6a0a822510036d16558213436bec469413ed7
Brandon Lingo <brandonlingo0@gmail.com>
2025-04-16 16:01:20 -0700

    add validation for both faculty and student login

--- BLL/app_utils.py
+++ BLL/app_utils.py
@@ -1,4 +1,4 @@
-from DAL.models import User, Student
+from DAL.models import User, Student, Faculty
 from DAL.database import db
 from flask import current_app
 
@@ -6,8 +6,17 @@ students = {
     0 :{
         "firstName":"Brandon",
         "lastName":"Lingo",
-        "email":"brandonlingo0@gmail.com",
+        "email":"brandon.lingo@student.csn.edu",
         "NSHEID":int("5008945170")
+        }
+}
+
+faculty = { 
+    0 :{
+        "firstName":"Karen",
+        "lastName":"Coombs",
+        "email":"karen.coombs@csn.edu",
+        "password":"password"
         }
 }
 
@@ -15,6 +24,7 @@ def createUsers(app):
 
     with app.app_context():
 
+        # add students
         for i in range(len(students)):
             new_user = Student(
                 firstName =  students[i]["firstName"],
@@ -26,12 +36,27 @@ def createUsers(app):
             db.session.add(new_user)
             db.session.commit()
 
+        # add faculty
+        for i in range(len(faculty)):
+            new_user = Faculty(
+                firstName =  faculty[i]["firstName"],
+                lastName  =  faculty[i]["lastName"],
+                email     =  faculty[i]["email"],
+                password  =  faculty[i]["password"]  # ❗ plain text password – only for testing!
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
 def validateEmail(email):
 
     #valid emails
     studentEmail = "@student.csn.edu"
-    facultyEmail = "@"
+    facultyEmail = "@csn.edu"
 
     atSign = email.find('@')
-    emailEnd = email[atSign:]   # splice email to get end
-    print(emailEnd)
+    emailEnd = email[atSign:].lower()   # splice email to get end
+    
+    if emailEnd == studentEmail: return 1
+    elif emailEnd == facultyEmail: return 2
+    else: return 0  #email is not valid
